0c7e5cd5fb3d477127bb053eced82661c98aac28
Ashura <ashuramajestic@gmail.com>
2024-05-05 12:59:25 +0530

    port

--- src/db/conn.js
+++ src/db/conn.js
@@ -3,6 +3,13 @@ const mongoose = require('mongoose');
 // mongoose.connect('mongodb://localhost:27017/react-practice-app-e-comm').then(() => {
 //     console.log('Connected DB');
 // });
-mongoose.connect('mongodb+srv://ashuramajestic:ashuramajestic@dashboard0.vxeivwn.mongodb.net/?retryWrites=true&w=majority&appName=DashBoard0').then(() => {
-    console.log('Connected DB');
+mongoose.set('strictQuery', false);
+
+
+mongoose.connect('mongodb+srv://ashuramajestic:ashuramajestic@dashboard0.vxeivwn.mongodb.net/?retryWrites=true&w=majority&appName=DashBoard0', {
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("Connection Created");
+}).catch((err) => {
+    console.log("Error: " + err);
 });
