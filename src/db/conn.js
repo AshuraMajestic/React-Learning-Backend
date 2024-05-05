c099ba98e2943ebceb4e951a5fc86333da8da529
Ashura <ashuramajestic@gmail.com>
2024-05-05 12:41:25 +0530

    database

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/react-practice-app-e-comm').then(() => {
+// mongoose.connect('mongodb://localhost:27017/react-practice-app-e-comm').then(() => {
+//     console.log('Connected DB');
+// });
+mongoose.connect('mongodb+srv://ashuramajestic:ashuramajestic@dashboard0.vxeivwn.mongodb.net/?retryWrites=true&w=majority&appName=DashBoard0').then(() => {
     console.log('Connected DB');
 });
